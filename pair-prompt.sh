e2284b24724af969d44735a744973ac27768386c
Eoin Coffey <ecoffey@gmail.com>
2012-06-04 09:42:35 -0600

    If no pair file, or email does not start with pair then just show email

--- pair-prompt.sh
+++ pair-prompt.sh
@@ -1,14 +1,25 @@
 PAIR_PROMPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 pair_prompt() {
-	if [ -f .pairs ]; then
+	tmp=$(git status)
+	if [ "$?" == "0" ]; then
+		local email=$(git config --get user.email)
 		local fmt="(%s)"
 		if [ -n "$1" ]; then
 			fmt="$1"
 		fi
-		local email=$(git config --get user.email)
+
 		if [ -n "$email" ]; then
-			local abbrevs=$(ruby $PAIR_PROMPT_DIR/pair_abbrev_from_email.rb $email)
-			printf -- "${fmt:-}" "$abbrevs"
+			if [ -f .pairs ]; then 
+				if [ "$email" == pair* ]; then
+					local abbrevs=$(ruby $PAIR_PROMPT_DIR/pair_abbrev_from_email.rb $email)
+					printf -- "${fmt:-}" "$abbrevs"
+				else
+					printf -- "${fmt:-}" "$email"
+				fi
+			else
+				printf -- "${fmt:-}" "$email"
+			fi
 		else
 			printf -- "${fmt:-}" "NO AUTHOR"
 		fi
